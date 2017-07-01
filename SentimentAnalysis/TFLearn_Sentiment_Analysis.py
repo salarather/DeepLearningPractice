74fa32a814c38ddd97e90e1b46b491ca28198324
Salar Ather <salar.ather@richmond.edu>
2017-07-01 05:36:21 +0500

    fine tuning hyperparamters - final accuracy 83%

--- SentimentAnalysis/TFLearn_Sentiment_Analysis.py
+++ SentimentAnalysis/TFLearn_Sentiment_Analysis.py
@@ -32,10 +32,7 @@ labels = pd.read_csv('labels.txt', header=None)
 
 # ### Counting word frequency
 # 
-# To start off we'll need to count how often each word appears in the data. We'll use this count to create a vocabulary we'll use to encode the review data. This resulting count is known as a [bag of words](https://en.wikipedia.org/wiki/Bag-of-words_model). We'll use it to select our vocabulary and build the word vectors. You should have seen how to do this in Andrew's lesson. Try to implement it here using the [Counter class](https://docs.python.org/2/library/collections.html#collections.Counter).
 # 
-# > **Exercise:** Create the bag of words from the reviews data and assign it to `total_counts`. The reviews are stores in the `reviews` [Pandas DataFrame](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html). If you want the reviews as a Numpy array, use `reviews.values`. You can iterate through the rows in the DataFrame with `for idx, row in reviews.iterrows():` ([documentation](http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.iterrows.html)). When you break up the reviews into words, use `.split(' ')` instead of `.split()` so your results match ours.
-
 # In[3]:
 
 
@@ -46,7 +43,8 @@ for _, row in reviews.iterrows():
 print("Total words in data set: ", len(total_counts))
 
 
-# Let's keep the first 10000 most frequent words. As Andrew noted, most of the words in the vocabulary are rarely used so they will have little effect on our predictions. Below, we'll sort `vocab` by the count value and keep the 10000 most frequent words.
+# Let's keep the first 10000 most frequent words. Most of the words in the vocabulary are rarely used so they will have little effect on our 
+# predictions. Below, we'll sort `vocab` by the count value and keep the 10000 most frequent words.
 
 # In[4]:
 
@@ -55,7 +53,6 @@ vocab = sorted(total_counts, key=total_counts.get, reverse=True)[:10000]
 print(vocab[:60])
 
 
-# What's the last word in our vocabulary? We can use this to judge if 10000 is too few. If the last word is pretty common, we probably need to keep more words.
 
 # In[5]:
 
@@ -63,15 +60,7 @@ print(vocab[:60])
 print(vocab[-1], ': ', total_counts[vocab[-1]])
 
 
-# The last word in our vocabulary shows up in 30 reviews out of 25000. I think it's fair to say this is a tiny proportion of reviews. We are probably fine with this number of words.
-# 
-# **Note:** When you run, you may see a different word from the one shown above, but it will also have the value `30`. That's because there are many words tied for that number of counts, and the `Counter` class does not guarantee which one will be returned in the case of a tie.
-# 
-# Now for each review in the data, we'll make a word vector. First we need to make a mapping of word to index, pretty easy to do with a dictionary comprehension.
-# 
-# > **Exercise:** Create a dictionary called `word2idx` that maps each word in the vocabulary to an index. The first word in `vocab` has index `0`, the second word has index `1`, and so on.
 
-# In[6]:
 
 
 word2idx = {word: i for i, word in enumerate(vocab)} ## create the word-to-index dictionary here
@@ -79,13 +68,8 @@ word2idx = {word: i for i, word in enumerate(vocab)} ## create the word-to-index
 
 # ### Text to vector function
 # 
-# Now we can write a function that converts a some text to a word vector. The function will take a string of words as input and return a vector with the words counted up. Here's the general algorithm to do this:
-# 
-# * Initialize the word vector with [np.zeros](https://docs.scipy.org/doc/numpy/reference/generated/numpy.zeros.html), it should be the length of the vocabulary.
-# * Split the input string of text into a list of words with `.split(' ')`. Again, if you call `.split()` instead, you'll get slightly different results than what we show here.
-# * For each word in that list, increment the element in the index associated with that word, which you get from `word2idx`.
-# 
-# **Note:** Since all words aren't in the `vocab` dictionary, you'll get a key error if you run into one of those words. You can use the `.get` method of the `word2idx` dictionary to specify a default returned value when you make a key error. For example, `word2idx.get(word, None)` returns `None` if `word` doesn't exist in the dictionary.
+# Now we can write a function that converts a some text to a word vector. The function will take a string of words as input and 
+# return a vector with the words counted up.
 
 # In[7]:
 
@@ -101,16 +85,7 @@ def text_to_vector(text):
     return np.array(word_vector)
 
 
-# If you do this right, the following code should return
-# 
-# ```
-# text_to_vector('The tea is for a party to celebrate '
-#                'the movie so she has no time for a cake')[:65]
-#                    
-# array([0, 1, 0, 0, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0,
-#        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-#        0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0])
-# ```       
+#    
 
 # In[8]:
 
@@ -136,10 +111,7 @@ for ii, (_, text) in enumerate(reviews.iterrows()):
 word_vectors[:5, :23]
 
 
-# ### Train, Validation, Test sets
-# 
-# Now that we have the word_vectors, we're ready to split our data into train, validation, and test sets. Remember that we train on the train data, use the validation data to set the hyperparameters, and at the very end measure the network performance on the test data. Here we're using the function `to_categorical` from TFLearn to reshape the target data so that we'll have two output units and can classify with a softmax activation function. We actually won't be creating the validation set here, TFLearn will do that for us later.
-
+# split the data into validation test and trraining sets
 # In[11]:
 
 
@@ -180,7 +152,7 @@ def build_model():
     # Output layer
     net = tflearn.fully_connected(net, 2, activation='softmax')
     net = tflearn.regression(net, optimizer='sgd', 
-                             learning_rate=0.05, 
+                             learning_rate=0.01, 
                              loss='categorical_crossentropy')
     
     model = tflearn.DNN(net)
@@ -200,22 +172,17 @@ model = build_model()
 
 
 # ## Training the network
-# 
-# Now that we've constructed the network, saved as the variable `model`, we can fit it to the data. Here we use the `model.fit` method. You pass in the training features `trainX` and the training targets `trainY`. Below I set `validation_set=0.1` which reserves 10% of the data set as the validation set. You can also set the batch size and number of epochs with the `batch_size` and `n_epoch` keywords, respectively. Below is the code to fit our the network to our word vectors.
-# 
-# You can rerun `model.fit` to train the network further if you think you can increase the validation accuracy. Remember, all hyperparameter adjustments must be done using the validation set. **Only use the test set after you're completely done training the network.**
 
 # In[24]:
 
 
 # Training
-model.fit(trainX, trainY, validation_set=0.1, show_metric=True, batch_size=128, n_epoch=10)
+model.fit(trainX, trainY, validation_set=0.1, show_metric=True, batch_size=128, n_epoch=200)
 
 
 # ## Testing
 # 
-# After you're satisified with your hyperparameters, you can run the network on the test set to measure its performance. Remember, *only do this after finalizing the hyperparameters*.
-
+# 
 # In[ ]:
 
 
